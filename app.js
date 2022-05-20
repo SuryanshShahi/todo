d75cc4ff3a1f012fc6ce5e31a2007a3cfa69460a
SuryanshShahi <suryansh06shahi@gmail.com>
2022-05-20 20:10:55 +0530

    todo

---
+++ app.js
@@ -0,0 +1,23 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+
+dotenv.config({ path: "./config.env" });
+app.use(express.json());
+
+require("./db/conn");
+app.use(require("./auth"));
+
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  // const path = require("path");
+  // app.get("*", (req, res) => {
+  //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // })
+}
+
+app.listen(PORT, () => {
+  console.log(`server is running at ${PORT}`);
+});
